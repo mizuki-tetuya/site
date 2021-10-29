93de1bb3088533d2e2a6f5e00d5b0fc8ad29570e
mizuki <mizukih130414@outlook.jp>
2021-10-29 23:11:44 +0900

    first-commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,76 @@
-function scrollTop(elem) {
-  const target = document.getElementsByClassName(elem);
-  target.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-}
+document.addEventListener('DOMContentLoaded', function(){
+	function menuClick() {
+		// クリックした際に実行する処理を記述
+		window.scrollTo({
+			top: 0,
+		});
+	}
+	// 引数に指定したclassの値をもつ要素をすべて取得
+	const menus = document.getElementsByClassName('news-item');
+	// 上記で取得したすべての要素に対してクリックイベントを適用
+	for(let i = 0; i < menus.length; i++) {
+  menus[i].addEventListener('click', menuClick, false);
+	}
+}, false);
 
-scrollTop('news-item');
+document.addEventListener('DOMContentLoaded', function(){
+	function menuClick() {
+		window.scrollTo({
+			top: 0,
+		});
+	}
+	const blogs = document.getElementsByClassName('blog-card-img');
+	for(let i = 0; i < blogs.length; i++) {
+  blogs[i].addEventListener('click', menuClick, false);
+	}
+}, false);
+
+document.addEventListener('DOMContentLoaded', function(){
+	function menuClick() {
+		window.scrollTo({
+			top: 0,
+		});
+	}
+	const blogTexts = document.getElementsByClassName('blog-text');
+	for(let i = 0; i < blogTexts.length; i++) {
+  blogTexts[i].addEventListener('click', menuClick, false);
+	}
+}, false);
+
+/*ニュース*/
+document.addEventListener('DOMContentLoaded', function(){
+	function menuClick() {
+		window.scrollTo({
+			top: 0,
+		});
+	}
+	const newsContents = document.getElementsByClassName('news-content-item');
+	for(let i = 0; i < newsContents.length; i++) {
+  newsContents[i].addEventListener('click', menuClick, false);
+	}
+}, false);
+
+/*社員ブログ*/
+document.addEventListener('DOMContentLoaded', function(){
+	function menuClick() {
+		window.scrollTo({
+			top: 0,
+		});
+	}
+	const blogImgs = document.getElementsByClassName('blog-content-img');
+	for(let i = 0; i < blogImgs.length; i++) {
+	blogImgs[i].addEventListener('click', menuClick, false);
+	}
+}, false);
+
+document.addEventListener('DOMContentLoaded', function(){
+	function menuClick() {
+		window.scrollTo({
+			top: 0,
+		});
+	}
+	const blogTexts = document.getElementsByClassName('blog-content-text');
+	for(let i = 0; i < blogTexts.length; i++) {
+	blogTexts[i].addEventListener('click', menuClick, false);
+	}
+}, false);
